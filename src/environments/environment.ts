2a0e356e3ce40b754b5775e46ea28fdb3739e735
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-01 12:52:08 +0300

    add the api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphyApiKey : "956apNyPs3c4t7aEBVXFrDHTPRSt7OXr"
 };
 
 /*
