ed14a12f01eedabe443af33738b7ef44c71a7720
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-01 10:41:36 +0300

    add the search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { GiphyComponent } from './giphy/giphy.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    SearchComponent,
+    GiphyComponent,
   ],
   imports: [
     BrowserModule,
