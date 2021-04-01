29b440ad09680317bbbd00c4a64bbd424786415b
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-01 12:52:25 +0300

    create a gif component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
 import { GiphyComponent } from './giphy/giphy.component';
 import { HeaderComponent } from './header/header.component';
+import { GifComponent } from './gif/gif.component';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { HeaderComponent } from './header/header.component';
     SearchComponent,
     GiphyComponent,
     HeaderComponent,
+    GifComponent,
   ],
   imports: [
     BrowserModule,
